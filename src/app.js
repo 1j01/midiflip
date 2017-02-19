d1069db711168a7a145f02335b94a66c4a363cca
Isaiah Odhner <isaiahodhner@gmail.com>
2017-02-19 22:19:49 +0000

    Make basic web interface

---
+++ src/app.js
@@ -0,0 +1,88 @@
+
+var drop_area_el = document.querySelector("body");
+var files_input_el = document.querySelector("#files-input");
+var output_el = document.querySelector("#output-list");
+var clear_results_el = document.querySelector("#clear-results");
+
+var fn = function(n){
+	return 127 - n;
+};
+
+var blob_urls = [];
+
+var clearResults = function(){
+	output_el.innerHTML = "";
+	for(var i=0; i<blob_urls.length; i++){
+		URL.revokeObjectURL(blob_urls[i]);
+	}
+};
+
+clear_results_el.addEventListener("click", clearResults)
+
+var addFile = function(file){
+	console.log("add file", file);
+	var file_reader = new FileReader();
+	file_reader.onload = function(){
+		try {
+			var result = midiflip(this.result, fn);
+			var result_blob = new Blob([result], {type: "audio/midi"});
+			var result_url = URL.createObjectURL(result_blob);
+			blob_urls.push(result_url);
+		} catch(e) {
+			var error = e;
+		}
+		var li = document.createElement("li");
+		output_el.appendChild(li);
+		var a = document.createElement("a");
+		li.appendChild(a);
+		a.textContent = file.name.replace(/\.midi?/, "");
+		a.download = file.name.replace(/\.midi?/, ".transformed.mid");
+		if(error){
+			li.classList.add("failed");
+			var error_el = document.createElement("div");
+			error_el.classList.add("error");
+			li.appendChild(error_el);
+			if(error.message.match("Invalid MIDIFileHeader")){
+				error_el.textContent = "Does not appear to be a MIDI file.";
+			}else{
+				error_el.textContent = error;
+			}
+		}else{
+			a.href = result_url;
+		}
+	};
+	file_reader.readAsArrayBuffer(file);
+};
+
+var addFiles = function(files){
+	console.log("add files", files);
+	for(var i=0; i<files.length; i++){
+		var file = files[i];
+		addFile(file);
+	}
+};
+
+drop_area_el.addEventListener('dragover', function (e) {
+	// console.log("dragover");
+	e.preventDefault();
+	e.dataTransfer.dropEffect = 'move';
+}, false);
+
+drop_area_el.addEventListener('dragenter', function (e) {
+	// console.log("dragenter");
+	e.preventDefault();
+	e.dataTransfer.dropEffect = 'move';
+}, false);
+
+drop_area_el.addEventListener('drop', function (e) {
+	console.log("drop");
+	e.stopPropagation();
+	e.preventDefault();
+	addFiles(e.dataTransfer.files);
+}, false);
+
+files_input_el.addEventListener("change", function(){
+	console.log("files inputted");
+	addFiles(this.files);
+});
+
--- src/midiflip.js
+++ src/midiflip.js
@@ -1,62 +1,78 @@
 
-var MIDIFile = require("midifile");
-var MIDIEvents = require("midievents");
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['midifile', 'midievents'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        
+		var MIDIFile = require("midifile");
+		var MIDIEvents = require("midievents");
 
-module.exports = function(arrayBuffer, fn, mess_with_percussion){
-
-	var midiFile = new MIDIFile(arrayBuffer);
-
-	// Reading headers
-	// midiFile.header.getFormat(); // 0, 1 or 2
-	// midiFile.header.getTracksCount(); // n
-	// Time division
-	// if(midiFile.header.getTimeDivision() === MIDIFile.Header.TICKS_PER_BEAT) {
-	// 	midiFile.header.getTicksPerBeat();
-	// } else {
-	// 	midiFile.header.getSMPTEFrames();
-	// 	midiFile.header.getTicksPerFrame();
-	// }
-
-	// MIDI events retriever
-	// var events = midiFile.getMidiEvents();
-	// // events[0].subtype; // type of MIDI event
-	// // events[0].playTime; // time in ms at wich the event must be played
-	// // events[0].param1; // first parameter
-	// // events[0].param2; // second one
-	// 
-	// for(var i=0; i<events.length; i++){
-	// 	var event = events[i];
-	// 	if(event.type === MIDIEvents.EVENT_MIDI_NOTE_OFF || event.type === MIDIEvents.EVENT_MIDI_NOTE_ON){
-	// 		console.log(event.param1);
-	// 		event.param1 = fn(event.param1);
-	// 	}
-	// }
+        module.exports = factory(MIDIFile, MIDIEvents);
+    } else {
+        // Browser globals (root is window)
+        root.midiflip = factory(root.MIDIFile, root.MIDIEvents);
+    }
+}(this, function (MIDIFile, MIDIEvents) {
+	return function(arrayBuffer, fn, mess_with_percussion){
 	
-	for(var track_index = 0; track_index < midiFile.tracks.length; track_index++){
-		// var rewritten = 0;
-		var tntmwp = false;
-		var events = midiFile.getTrackEvents(track_index);
-		for(var i=0; i<events.length; i++){
-			var event = events[i];
-			if(event.type === MIDIEvents.EVENT_MIDI){
-				if(event.subtype === MIDIEvents.EVENT_MIDI_NOTE_OFF || event.subtype === MIDIEvents.EVENT_MIDI_NOTE_ON){
-					// console.log(event.subtype == event.type, event.channel);
-					var isPercussion = event.channel === 10;
-					if(!isPercussion || mess_with_percussion){
-						event.param1 = fn(event.param1, {channel: event.channel, isPercussion: isPercussion});
-					}else{
-						tntmwp = true;
+		var midiFile = new MIDIFile(arrayBuffer);
+	
+		// Reading headers
+		// midiFile.header.getFormat(); // 0, 1 or 2
+		// midiFile.header.getTracksCount(); // n
+		// Time division
+		// if(midiFile.header.getTimeDivision() === MIDIFile.Header.TICKS_PER_BEAT) {
+		// 	midiFile.header.getTicksPerBeat();
+		// } else {
+		// 	midiFile.header.getSMPTEFrames();
+		// 	midiFile.header.getTicksPerFrame();
+		// }
+	
+		// MIDI events retriever
+		// var events = midiFile.getMidiEvents();
+		// // events[0].subtype; // type of MIDI event
+		// // events[0].playTime; // time in ms at wich the event must be played
+		// // events[0].param1; // first parameter
+		// // events[0].param2; // second one
+		// 
+		// for(var i=0; i<events.length; i++){
+		// 	var event = events[i];
+		// 	if(event.type === MIDIEvents.EVENT_MIDI_NOTE_OFF || event.type === MIDIEvents.EVENT_MIDI_NOTE_ON){
+		// 		console.log(event.param1);
+		// 		event.param1 = fn(event.param1);
+		// 	}
+		// }
+		
+		for(var track_index = 0; track_index < midiFile.tracks.length; track_index++){
+			// var rewritten = 0;
+			var tntmwp = false;
+			var events = midiFile.getTrackEvents(track_index);
+			for(var i=0; i<events.length; i++){
+				var event = events[i];
+				if(event.type === MIDIEvents.EVENT_MIDI){
+					if(event.subtype === MIDIEvents.EVENT_MIDI_NOTE_OFF || event.subtype === MIDIEvents.EVENT_MIDI_NOTE_ON){
+						// console.log(event.subtype == event.type, event.channel);
+						var isPercussion = event.channel === 10;
+						if(!isPercussion || mess_with_percussion){
+							event.param1 = fn(event.param1, {channel: event.channel, isPercussion: isPercussion});
+						}else{
+							tntmwp = true;
+						}
+						// rewritten++;
 					}
-					// rewritten++;
 				}
 			}
+			if(tntmwp){
+				console.log("Tried not to mess with percussion");
+			}
+			midiFile.setTrackEvents(track_index, events);
+			// console.log("(Modified " + rewritten + " of " + events.length + " events in track " + track_index + ")");
 		}
-		if(tntmwp){
-			console.log("Tried not to mess with percussion");
-		}
-		midiFile.setTrackEvents(track_index, events);
-		// console.log("(Modified " + rewritten + " of " + events.length + " events in track " + track_index + ")");
-	}
-	
-	return midiFile.getContent();
-};
+		
+		return midiFile.getContent();
+	};
+}));
