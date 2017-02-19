0902268eb683919d41639c994b80ce65b5da4773
Isaiah Odhner <isaiahodhner@gmail.com>
2017-02-19 23:49:15 +0000

    Improve file selection UI

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 
 var drop_area_el = document.querySelector("body");
-var files_input_el = document.querySelector("#files-input");
+var select_files_el = document.querySelector("#select-files");
 var output_el = document.querySelector("#output-list");
 var clear_results_el = document.querySelector("#clear-results");
 
@@ -18,6 +18,18 @@ var clearResults = function(){
 };
 
 clear_results_el.addEventListener("click", clearResults);
+
+var selectFiles = function(){
+	var files_input_el = document.createElement("input");
+	files_input_el.setAttribute("type", "file");
+	files_input_el.setAttribute("multiple", "multiple");
+	files_input_el.addEventListener("change", function(){
+		addFiles(this.files);
+	});
+	files_input_el.click();
+};
+
+select_files_el.addEventListener("click", selectFiles);
 
 var addFile = function(file){
 	var file_reader = new FileReader();
@@ -76,7 +88,4 @@ drop_area_el.addEventListener('drop', function (e) {
 	addFiles(e.dataTransfer.files);
 }, false);
 
-files_input_el.addEventListener("change", function(){
-	addFiles(this.files);
-});
 
