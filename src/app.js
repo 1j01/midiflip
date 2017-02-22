2af8ba620d23a07a403098918aa2e387f7e88308
Isaiah Odhner <isaiahodhner@gmail.com>
2017-02-22 06:27:47 +0000

    Clean up

--- src/app.js
+++ src/app.js
@@ -7,22 +7,19 @@ var results_zipped_container_el = document.querySelector("#results-zipped-contai
 var results_container_el = document.querySelector("#results-container");
 var clear_results_el = document.querySelector("#clear-results");
 
-var fn = function(n){
+var fn = function(n) {
 	return 127 - n;
 };
 
 var results = [];
-// var result_blobs_by_file_name = {};
-var blob_urls = [];
 var zip_blob_url = null;
 
-var clearResults = function(){
-	for(var i=0; i<blob_urls.length; i++){
-		URL.revokeObjectURL(blob_urls[i]);
+var clear_results = function() {
+	for (var i = 0; i < results.length; i++) {
+		URL.revokeObjectURL(results[i].blob_url);
 	}
-	// result_blobs_by_file_name = {};
+	URL.revokeObjectURL(zip_blob_url);
 	results = [];
-	blob_urls = [];
 	zip_blob_url = null;
 	results_container_el.setAttribute("hidden", "hidden");
 	results_zipped_container_el.setAttribute("hidden", "hidden");
@@ -30,27 +27,26 @@ var clearResults = function(){
 	output_zipped_el.innerHTML = "";
 };
 
-clearResults();
-clear_results_el.addEventListener("click", clearResults);
+clear_results_el.addEventListener("click", clear_results);
 
-var selectFiles = function(){
+var select_files = function() {
 	var files_input_el = document.createElement("input");
 	files_input_el.setAttribute("type", "file");
 	files_input_el.setAttribute("multiple", "multiple");
-	files_input_el.addEventListener("change", function(){
-		addFiles(this.files);
+	files_input_el.addEventListener("change", function() {
+		add_files(this.files);
 	});
 	files_input_el.click();
 };
 
-select_files_el.addEventListener("click", selectFiles);
+select_files_el.addEventListener("click", select_files);
 
-var addFile = function(file, callback){
+var add_file = function(file, callback) {
 	
 	var result = {file_name: file.name.replace(/\.midi?/, ".transformed.mid")};
-	results = results.filter(function(old_result){
+	results = results.filter(function(old_result) {
 		var match = old_result.file_name == result.file_name
-		if(match){
+		if (match) {
 			old_result.element.parentElement.removeChild(old_result.element);
 			return false;
 		}
@@ -68,75 +64,63 @@ var addFile = function(file, callback){
 	result.element = li;
 	
 	var file_reader = new FileReader();
-	file_reader.onload = function(){
+	file_reader.onload = function() {
 		try {
 			var result_array_buffer = midiflip(this.result, fn);
 			result.blob = new Blob([result_array_buffer], {type: "audio/midi"});
 			result.blob_url = URL.createObjectURL(result.blob);
-			blob_urls.push(result.blob_url);
 		} catch(e) {
 			result.error = e;
 		}
-		if(result.error){
+		if (result.error) {
 			li.classList.add("failed");
 			var error_el = document.createElement("div");
 			error_el.classList.add("error");
 			li.appendChild(error_el);
-			if(result.error.message.match("Invalid MIDIFileHeader")){
+			if (result.error.message.match("Invalid MIDIFileHeader")) {
 				error_el.textContent = "Does not appear to be a MIDI file.";
-			}else{
+			} else {
 				error_el.textContent = result.error;
 			}
-			// callback(result.error); // nope
-		}else{
+		} else {
 			a.href = result.blob_url;
-			// result_blobs_by_file_name[a.download] = result_blob;
-			// callback(null);
 		}
 		callback();
 	};
 	file_reader.readAsArrayBuffer(file);
 };
 
-var addFiles = function(files){
-	if(files.length == 0){
+var add_files = function(files) {
+	if (files.length == 0) {
 		return;
 	}
 	results_container_el.removeAttribute("hidden");
-	// for(var i=0; i<files.length; i++){
-	// 	var file = files[i];
-	// 	addFile(file);
-	// }
-	async.each(files, addFile, function(err){
-		// console.log("done, make zip?", results, results.some(function(result){ return result.blob; }));
-		// if(Object.keys(result_blobs_by_file_name).length > 1){
-		// var n_result_blobs = results.reduce(function(acc, result) {
-		// 	return acc + !!result.blob;
-		// }, 0);
-		if(results.some(function(result){ return result.blob; })){
-		// if(n_result_blobs > 1){
-			createZip();
+	async.each(files, add_file, function(err) {
+		// NOTE: we create a zip file if there's only one file
+		// for feature visibility, and general consistency
+		if (results.some(function(result) { return result.blob; })) {
+			create_zip();
 		}
 	});
 };
 
-drop_area_el.addEventListener('dragover', function (e) {
+drop_area_el.addEventListener("dragover", function (e) {
 	e.preventDefault();
-	e.dataTransfer.dropEffect = 'copy';
+	e.dataTransfer.dropEffect = "copy";
 }, false);
 
-drop_area_el.addEventListener('dragenter', function (e) {
+drop_area_el.addEventListener("dragenter", function (e) {
 	e.preventDefault();
-	e.dataTransfer.dropEffect = 'copy';
+	e.dataTransfer.dropEffect = "copy";
 }, false);
 
-drop_area_el.addEventListener('drop', function (e) {
+drop_area_el.addEventListener("drop", function (e) {
 	e.stopPropagation();
 	e.preventDefault();
-	addFiles(e.dataTransfer.files);
+	add_files(e.dataTransfer.files);
 }, false);
 
-var createZip = function(){
+var create_zip = function() {
 	
 	URL.revokeObjectURL(zip_blob_url);
 	output_zipped_el.innerHTML = "";
@@ -154,35 +138,28 @@ var createZip = function(){
 	
 	// use a BlobWriter to store the zip into a Blob object
 	zip.createWriter(new zip.BlobWriter(), function(writer) {
-		// for(var file_name in result_blobs_by_file_name){
-		// 	var file = result_blobs_by_file_name[file_name];
-		// 	to_write.push({file_name: file_name, file: file});
-		// }
-		var to_write = results.filter(function(result){ return result.blob; });
+		var to_write = results.filter(function(result) { return result.blob; });
 		var to_write_index = 0;
-		var write_remaining_files = function(){
-			
+		var write_remaining_files = function() {
 			var item = to_write[to_write_index];
-			if(item){
+			if (item) {
 				writer.add(item.file_name, new zip.BlobReader(item.blob), function() {
 					// onsuccess callback
 					to_write_index++;
 					write_remaining_files();
-				}, function(currentIndex, totalIndex) {
+				}, function(current_index, total_index) {
 					// onprogress callback
-					progress_bar.value = (to_write_index + currentIndex / totalIndex) / to_write.length;
+					progress_bar.value = (to_write_index + current_index / total_index) / to_write.length;
 				});
-			}else{
+			} else {
 				// finish writing
 				writer.close(function(zip_blob) {
 					zip_blob_url = URL.createObjectURL(zip_blob);
-					blob_urls.push(zip_blob_url);
 					progress_bar.parentElement.removeChild(progress_bar);
 					a.href = zip_blob_url;
 				});
 			}
 		};
-		
 		write_remaining_files();
 	}, function(error) {
 		// onerror callback
